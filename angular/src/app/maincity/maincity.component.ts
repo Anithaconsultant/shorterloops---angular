97a4a5123ad1dfbe7bad052e0d6d98ff329c12fd
Anithaconsultant <anitha@iihs.ac.in>
2023-05-09 12:16:19 +0530

    cityscape and displaying cartid is done

--- angular/src/app/maincity/maincity.component.ts
+++ angular/src/app/maincity/maincity.component.ts
@@ -2,7 +2,7 @@ import { Component, HostListener, ViewChild, AfterViewInit, ElementRef, OnInit }
 import * as $ from 'jquery';
 import panzoom from "panzoom";
 import { LoginserviceService } from '../services/loginservice.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-maincity',
   templateUrl: './maincity.component.html',
@@ -10,15 +10,35 @@ import { LoginserviceService } from '../services/loginservice.service';
 })
 
 export class MaincityComponent implements AfterViewInit, OnInit {
+
   public instance: any;
   currentUserRole:string='';
+  currentUserCartId='';
   positionObject = {
-    'House6': [[-529, -3505],[707, 3897]],
-    'Recycling truck': [[-5762, -3505],[707, 3897]],
-    'Supermarket': [[-529, -3505],[707, 3897]],
+    'House6':  [[ -199, -3525],[712, 3921],[665, 3814],[628, 3726]],
+    'House7':  [[ -443, -3538],[1049, 3921],[1003, 3814],[967, 3726]],
+    'House8':  [[ -744, -3475],[1384, 3921],[1337, 3814],[1303, 3726]],
+    'House9':  [[-1053, -3468],[1718, 3921],[1674, 3814],[1638, 3726]],
+    'House10': [[-1282, -3537],[2061, 3921],[2009, 3814],[1978, 3726]],
+    'House1':  [[ -199, -3154],[712, 3584],[665, 3459],[628, 3385]],
+    'House2':  [[ -443, -3154],[1049, 3584],[1003, 3459],[967, 3385]],
+    'House3':  [[ -744, -3154],[1384, 3584],[1337, 3459],[1303, 3385]],
+    'House4':  [[-1053, -3154],[1718, 3584],[1674, 3459],[1638, 3385]],
+    'House5': [[-1282,-3154],[2061, 3584],[2009, 3459],[1978, 3385]],
+    'Recycling truck': [[-5762, -3505],[5930, 3921],[5898, 3814],[6023, 3726]],
+    'Supermarket': [[-5762, -3505],[6268, 3921],[6235, 3814],[6361, 3726]],
+    'Grocery shop': [[-6158, -3490],[6617, 3921],[6576, 3814],[6698, 3726]],
+    'Ubottle': [[-6254, -3485],[6958, 3921],[6910, 3814],[7034, 3726]],
+    'Making plant': [[-6564, -3510],[7295, 3921],[7246, 3814],[7371, 3726]],
+    'Reverse Vending Machine': [[-6635, -3154],[7295, 3584],[7246, 3459],[7371, 3385]],
+    'Plastic Recycling plant': [[-6254, -3154],[6958, 3584],[6910, 3459],[7034, 3385]],
+    'Refilling Van': [[-6158, -3154],[6617, 3584],[6576, 3459],[6698, 3385]],
+    'Bottle collection truck': [[-5762, -3154],[6279, 3584],[6235, 3459],[6361, 3385]],
+    'Mayor': [[-5762, -3154],[6268, 3584],[6235, 3459],[6361, 3385]]
   }
-  constructor(private logser: LoginserviceService) {
+  constructor(private logser: LoginserviceService,private router:Router) {
     this.currentUserRole=this.logser.currentuser.Role;
+    this.currentUserCartId=this.logser.currentuser.cartId;
   }
 
   @HostListener('document:keydown.arrowdown', ['$event'])
@@ -62,7 +82,9 @@ export class MaincityComponent implements AfterViewInit, OnInit {
 
 
   ngOnInit(): void {
-
+   if(this.logser.currentuser.Username==''){
+    this.router.navigate(["login"])
+   }
   }
   @ViewChild('maincity', { static: false })
   private scene!: ElementRef;
@@ -79,12 +101,19 @@ export class MaincityComponent implements AfterViewInit, OnInit {
     });
   
     let a=this.currentUserRole;
+    console.log(a);
     let x=this.positionObject[a as keyof typeof this.positionObject][0][0];
     let y=this.positionObject[a as keyof typeof this.positionObject][0][1];
     let x1=this.positionObject[a as keyof typeof this.positionObject][1][0];
     let y1=this.positionObject[a as keyof typeof this.positionObject][1][1];
+    let x2=this.positionObject[a as keyof typeof this.positionObject][2][0];
+    let y2=this.positionObject[a as keyof typeof this.positionObject][2][1];
+    let x3=this.positionObject[a as keyof typeof this.positionObject][3][0];
+    let y3=this.positionObject[a as keyof typeof this.positionObject][3][1];
     this.dopanzoom(x, y, '1');
     $(".cart").css({ 'left': x1+'px', 'top':y1+'px' });
+    $(".housedisplay").css({ 'left': x2+'px', 'top':y2+'px' }).html(a+'<br/>cart ID : '+this.currentUserCartId);
+    $(".displaypanel").css({ 'left': x3+'px', 'top':y3+'px' }).html('2000');
   }
 
 }
