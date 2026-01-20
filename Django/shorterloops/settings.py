994d68271e6fa7624d11fe71a97bf83e8c235255
Anitha Saravanan <anitha.saravanan@iihs.in>
2026-01-20 11:17:02 +0530

    Update-removed venv folder from github

--- Django/shorterloops/settings.py
+++ Django/shorterloops/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'shorterloop',
-            'USER': 'root',
+            'USER': 'shorterloops',
             'PASSWORD': 'Admin@1234',
             'HOST': '127.0.0.1',
             'PORT': '3306',
